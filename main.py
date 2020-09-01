60b10c1e8f67fea9738a8d8f8525cc41584ed175
slavah8 <vhq5091@psu.edu>
2020-09-01 23:02:31 +0000

    added spaces and periods

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 temperature=float(input("Enter temperature:"))
-unitinput=str(input("Enter unit in F/f or C/c:"))
+unitinput=str(input(" Enter unit in F/f or C/c: "))
 
 if unitinput == "C" or unitinput == "c":
   temperature2 = (temperature * 9/5) + 32
-  print(f"{temperature}° in Celsius is equivalent to {temperature2}° Fahrenheit")
+  print(f"{temperature}° in Celsius is equivalent to {temperature2}° Fahrenheit.")
 elif unitinput == "F" or unitinput == "f":
   temperature2 = (temperature - 32) / 9.0 * 5
-  print(f"{temperature}° in Fahrenehit is equivalent to {temperature2}° Celsius")
+  print(f"{temperature}° in Fahrenehit is equivalent to {temperature2}° Celsius.")
 else:
-  print(f"Invalid unit({unitinput})")
+  print(f"Invalid unit({unitinput}).")
 
